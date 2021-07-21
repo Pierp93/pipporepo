8f1f2b43b99dba1aadc76921e3df6a2d533c4498
proprietario <proprietario@192.168.1.17>
2021-07-22 00:25:39 +0200

    PRIMO COMMIT

---
+++ src/com/example/test/MyTest.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MyTest {
+
+		private static final Logger l = LogManager.getLogger(MyTest.class);
+		
+		public  void MyTest() {
+			
+			
+			l.trace("Trace msg");
+			l.debug("Debug msg");
+			l.info("Info msg");
+			l.warn("Warn msg");
+			l.error("Error msg");
+			l.fatal("Fatal msg");
+			
+			
+			
+		}
+}
---
+++ src/com/example/test/test/MyTestTest.java
@@ -0,0 +1,20 @@
+package com.example.test.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.test.MyTest;
+
+class MyTestTest {
+
+	@Test
+	void test() {
+		MyTest newTest = new MyTest(); 
+		
+		newTest.MyTest();
+		
+
+	}
+
+}
